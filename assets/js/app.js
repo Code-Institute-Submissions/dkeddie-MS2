5c9a90c938220788c229a8f236e83d0ee3e737d0
dkeddie <mail@davidkeddie.com>
2020-10-17 22:54:05 +0100

    Meteostat API integration and Average Temperature rotation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,97 +1,61 @@
 // DESTINATION SEARCH
 
-let currentLocation = "London";
+// let currentLocation
 let destination
 
-console.log(destination)
-
-
-
-
-
-
-// newFunction();
-
 // Algolia Places Search: https://www.algolia.com/
-
 function autoComplete() {
-  // src = "https://cdn.jsdelivr.net/npm/places.js@1.19.0";
   var placesAutocomplete = places({
     appId: 'pl3QLZ5PGJOK',
     apiKey: '17ed7fe7c8a13ca6a53f86095b7cde31',
     container: document.querySelector('#destination')
   });
 }
-
 autoComplete();
 
+// Get Current Location / Coordinates
 
+let cLat;
+let cLng;
+
+if ('geolocation' in navigator) {
+  console.log('gelocation available');
+  navigator.geolocation.getCurrentPosition(position => {
+    cLat = position.coords.latitude;
+    cLng = position.coords.longitude;
+  });
+}
+else {
+  console.log('gelocation not available')
+}
+
+// Load Current Location and Destination to variables
 function getDestination() {
+  // Destination
   document.getElementById('destination').addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
       destination = document.getElementById('destination').value;
       console.log(destination)
-    }
-  })
-};
+      getData()
+      setTimeout(() => {
+        getWeather()
+      }, 200);
 
+    }
+  });
+  // Current Location
+  setTimeout(async () => {
+    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${cLat},${cLng}&key=AIzaSyBKd5I7u1oc_iX8wrBze-LNNmiHFPqdtCI`);
+    let location = await response.json();
+    address = location.results[6].formatted_address
+    console.log(address)
+  }, 200);
+};
 getDestination()
 
-// let destination = document.getElementById('destination').addEventListener('keydown', (e) => {
-//   if (e.keyCode === 13) {
-//     destination = document.getElementById('destination').value;
-//   }
-// })
-
-// document.getElementById('locations').onsubmit = async (e) => {
-//   e.preventDefault();
-
-//   let response = await fetch('/article/formdata/post/user', {
-//     method: 'POST',
-//     body: new FormData(locations)
-//   });
-
-//   let result = await response.json();
-
-//   alert(result.message);
-// };
-
-// let destination = await document.getElementById('destination').value;
-
-// async function latestLocation() {
-//   let destination = await document.getElementById('destination').value;
-//   const dest = await document.getElementById('destination').addEventListener('keydown', (e) => {
-//     if (e.keyCode === 13) {
-//       console.log(destination);
-//     }
-//   })
-// }
-
-
-//   latestLocation()
-
-
-// async function getDestination () {
-//   const travelDest = await destination;
-//   console.log (destination)
-//   console.log(travelDest)
-// }
-
-// async function getLocation() {
-//   const travelDest = await document.getElementById('destination').addEventListener('keydown', (e) => {
-//     if (e.keyCode === 13) {
-//       travelDest = document.getElementById('destination').value;
-//       // return
-//     }
-//     // return;
-//     // console.log(destination)
-
-//   })
-// }
-
-// console.log(destination)
-
-// getLocation();
+setTimeout(() => {
+  console.log(destination)
+}, 200);
 
 // MONTH SELECTOR TILE
 
@@ -110,8 +74,10 @@ function newFunction() {
   autoComplete();
 }
 
+
+let m;
 function showMonth() {
-  let m = 0;
+  m = 0;
   document.getElementById("currentMonth").innerHTML = months[monthArray[m].getMonth()];
   document.getElementById("monthForward").addEventListener('click', function () {
     if (m <= 10) {
@@ -136,3 +102,90 @@ function showMonth() {
 showMonth();
 
 // Metostat API Fetch
+
+const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+const key = '&key=AIzaSyBKd5I7u1oc_iX8wrBze-LNNmiHFPqdtCI';
+
+
+let dLat
+let dLng
+// let address = 0
+// let place = 0
+
+async function getData() {
+  // address = await document.getElementById('City').value;
+  // console.log(address)
+  const response = await fetch(`${url}${destination}${key}`);
+  console.log(`${url}${destination}${key}`)
+  const locationData = await response.json();
+  console.log(locationData)
+  dLat = locationData.results[0].geometry.location.lat
+  dLng = locationData.results[0].geometry.location.lng
+  // place = locationData.results[0].formatted_address
+  // console.log(place)
+};
+
+// setTimeout(() => {
+//   getData()
+//   console.log(`${url}${destination}${key}`)
+// }, 1000);
+
+
+// Weather DATA Import
+
+
+let weatherData = []
+let weatherDataTemps = []
+
+let urlWeather = 0
+
+async function getWeather() {
+  // const coordinates = await getData
+  urlWeather = `https://api.meteostat.net/v2/point/climate?lat=${dLat}&lon=${dLng}`
+  console.log(urlWeather)
+  const response = await fetch(urlWeather, {
+    method: 'GET',
+    credentials: 'omit',
+    headers: {
+      // 'Content-Type': 'application/json',
+      // 'x-api-key': 'RMs9pME8PJQpNfti54tiw4fJQOquTm71',
+    },
+  })
+  const data1 = await response.json()
+  console.log(data1)
+  weatherData = data1.data
+  // weatherDataMonths = weatherData.map((months, index) => {
+  //   return weatherData[index].month;
+  // })
+  weatherDataTemps = weatherData.map((months, index) => {
+    return weatherData[index].tavg;
+  })
+  console.log(months)
+  console.log(monthArray)
+  console.log(weatherData)
+  console.log(weatherDataTemps[m])
+  function weatherUpdate() {
+    const mo = document.getElementById('currentMonth').innerHTML;
+    const moInt = months.indexOf(mo);
+    const moTemp = weatherData[moInt].tavg
+    console.log(mo)
+    console.log(moInt)
+    console.log(moTemp)
+    document.getElementById('temp').innerHTML = `${moTemp}&degC`;
+    
+  }
+  weatherUpdate()
+}
+
+  // setTimeout(() => {
+
+  // }, 250);
+  
+// };
+
+document.getElementById('month').addEventListener('click', () => {
+  const mo = document.getElementById('currentMonth').innerHTML
+  const moInt = months.indexOf(mo);
+  const moTemp = weatherData[moInt].tavg
+  document.getElementById('temp').innerHTML = `${moTemp}&degC`
+})
